bd659b7f355ef6d59ce55aca82167cf5de0cc490
kenranunderscore <johb.maier@gmail.com>
2018-01-01 14:30:11 +0100

    Add basic Wave class

---
+++ src/main/java/kenran/util/Wave.java
@@ -0,0 +1,12 @@
+package kenran.util;
+
+import java.awt.geom.Point2D;
+
+public class Wave {
+    public Point2D.Double firePosition;
+    public long fireTime;
+    public double bulletVelocity;
+    public double angle;
+    public double traveledDistance;
+    public int direction;
+}
