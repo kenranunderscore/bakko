c59f3e488fbe245b7ee181782e0b90b36f0e11e6
kenranunderscore <johb.maier@gmail.com>
2018-01-03 23:17:30 +0100

    Normalize PM data

--- src/main/java/kenran/util/MovementState.java
+++ src/main/java/kenran/util/MovementState.java
@@ -1,17 +1,23 @@
 package kenran.util;
 
+import robocode.Rules;
+
 public class MovementState {
     private final double _turnRate;
     private final double _velocity;
+    private final double _normalizedTurnRate;
+    private final double _normalizedVelocity;
 
     public MovementState(double turnRate, double velocity) {
         this._turnRate = turnRate;
         this._velocity = velocity;
+        _normalizedTurnRate = turnRate / Rules.MAX_TURN_RATE;
+        _normalizedVelocity = velocity / Rules.MAX_VELOCITY;
     }
 
     public double compare(MovementState other) {
-        double turnRateDifference = this._turnRate - other._turnRate;
-        double velocityDifference = this._velocity - other._velocity;
+        double turnRateDifference = this._normalizedTurnRate - other._normalizedTurnRate;
+        double velocityDifference = this._normalizedVelocity - other._normalizedVelocity;
         return Math.abs(turnRateDifference) + Math.abs(velocityDifference);
     }
 
